66d1ed35007ea86941b7f788431bb8c0eecc85f8
Kishan <kishanland@gmail.com>
2020-03-11 01:09:06 +0530

    Splash screen added. New icon for the app. ActionBar was removed to improve readability.

---
+++ app/src/main/java/echo/m/demoapp/SplashActivity.java
@@ -0,0 +1,26 @@
+package echo.m.demoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+}
