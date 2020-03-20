aa34bed55a03334fed248179cd02e77a84479402
Kishan <kishanland@gmail.com>
2020-03-21 02:38:11 +0530

    Splash update. Minor

--- app/src/main/java/echo/m/demoapp/SplashActivity.java
+++ app/src/main/java/echo/m/demoapp/SplashActivity.java
@@ -22,6 +22,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
-        }, secondsDelayed * 3500);
+        }, secondsDelayed * 3000);
     }
 }
