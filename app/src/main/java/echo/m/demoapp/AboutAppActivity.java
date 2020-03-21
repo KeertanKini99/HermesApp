637ff8dc215a5d33d1f30bcbe620810b2cec959c
Kishan <kishanland@gmail.com>
2020-03-21 15:40:54 +0530

    About App page designed.

--- app/src/main/java/echo/m/demoapp/AboutAppActivity.java
+++ app/src/main/java/echo/m/demoapp/AboutAppActivity.java
@@ -1,14 +1,49 @@
 package echo.m.demoapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutAppActivity extends AppCompatActivity {
 
+    Button update;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
+
+        update = (Button) findViewById(R.id.update);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder
+                        = new AlertDialog
+                        .Builder(getApplicationContext());
+                builder.setMessage("App is already up to date.");
+                builder.setTitle("Check for Updates");
+
+                builder.setCancelable(false);
+                builder
+                        .setPositiveButton(
+                                "Ok",
+                                new DialogInterface
+                                        .OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog,
+                                                        int which)
+                                    {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
     }
 }
