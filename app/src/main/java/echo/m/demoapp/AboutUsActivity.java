9ddf014299d60a3fdf901ce4b8b707eadb00a3c4
Kishan <kishanland@gmail.com>
2020-03-21 16:15:10 +0530

    About us page designed.

--- app/src/main/java/echo/m/demoapp/AboutUsActivity.java
+++ app/src/main/java/echo/m/demoapp/AboutUsActivity.java
@@ -2,13 +2,39 @@ package echo.m.demoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class AboutUsActivity extends AppCompatActivity {
 
+    ImageButton kishan, keertan;
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        kishan = (ImageButton) findViewById(R.id.kishanButton);
+        keertan = (ImageButton) findViewById(R.id.keertanButton);
+
+        kishan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.setContentView(R.layout.prof_kishan);
+                myDialog.setCanceledOnTouchOutside(true);
+            }
+        });
+
+        keertan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.setContentView(R.layout.prof_keertan);
+                myDialog.setCanceledOnTouchOutside(true);
+            }
+        });
     }
+
+
 }
