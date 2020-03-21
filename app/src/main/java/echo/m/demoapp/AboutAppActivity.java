01eacf575cefee025a373aadbbf0356d0a5c75d0
Kishan <kishanland@gmail.com>
2020-03-21 20:22:04 +0530

    Back button fix.

--- app/src/main/java/echo/m/demoapp/AboutAppActivity.java
+++ app/src/main/java/echo/m/demoapp/AboutAppActivity.java
@@ -1,10 +1,12 @@
 package echo.m.demoapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -51,6 +53,7 @@ public class AboutAppActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        this.finishAffinity();
         Intent i = new Intent(AboutAppActivity.this, MainActivity.class);
         startActivity(i);
     }
